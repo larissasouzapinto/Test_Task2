7d8d0c766f0c139310150e6d782bf57fa69dbdd5
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-02-26 16:58:17 -0300

    update 3

--- teste1.groovy
+++ teste1.groovy
@@ -1,9 +1,28 @@
 node('master'){
-
-    stage('Stage 1') {
-        checkout scm
-       // sh './mvnw clean compile package test'
-        echo 'Hello world!' 
-
-    }
-}   
+try{
+       stage('Limpeza do cache') {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
+ 
+       }
+ 
+        stage('Compilação do maven e Teste'){
+            checkout scm
+            sh './mvnw clean compile package test'
+ 
+        }
+    catch (exec)
+    {
+        currentBuild.result = 'FAILURE'
+        throw new Exception(exec)
+        
+    }finally{
+       
+       
+    } 
+}
+}
