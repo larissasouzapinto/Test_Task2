b093b4de9a3ac9d6a999c85baec9fde3c1d4cdad
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-01 16:39:49 -0300

    update teste sonarqube

--- teste1.groovy
+++ teste1.groovy
@@ -27,9 +27,9 @@ node('master'){
        
     } 
     stage('Teste Sonarqube'){
-      sh 'mvn sonar:sonar \'
-      sh '-Dsonar.projectKey=projetotask2 \'  
-      sh '-Dsonar.host.url=http://localhost:9000 \'
+      sh 'mvn sonar:sonar'
+      sh '-Dsonar.projectKey=projetotask2'  
+      sh '-Dsonar.host.url=http://localhost:9000'
       sh '-Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46'
     }
 }
