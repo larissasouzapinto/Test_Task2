7e116d00a42c27309ab5889c33d8b2eff91677de
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-01 13:36:36 -0300

    Teste sonar 1

--- teste1.groovy
+++ teste1.groovy
@@ -30,8 +30,21 @@ node('master'){
        
     } 
 
-    stage('Unit tests') {
-        sh 'mvn clean test'
-    }
+        stage('Build') {
+            withSonarQubeEnv('sonar') {
+            def mvnHome = tool 'maven-3.5.0'
+            sh "'${mvnHome}/bin/mvn' -f backend/pom.xml clean package sonar:sonar"
+            }
+        }
+        
+
+        stage("Quality Gate") { 
+            timeout(time: 5, unit: 'MINUTES') { 
+                def qualityGate = waitForQualityGate() 
+                if (qualityGate.status != 'OK') {
+                    error "O código não está de acordo com as regras do Sonar: ${qualityGate.status}"
+                }
+            }
+        }
 
 }
