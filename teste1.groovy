830588aff02d99de9e49baff29a7a02342d50f0b
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-01 13:55:18 -0300

    sonarqubeenv

--- teste1.groovy
+++ teste1.groovy
@@ -32,12 +32,14 @@ node('master'){
     } 
 
        stage("Quality Gate") { 
+           withSonarQubeEnv('sonar') {
             timeout(time: 5, unit: 'MINUTES') { 
                 def qualityGate = waitForQualityGate() 
                 if (qualityGate.status != 'OK') {
                     error "O código não está de acordo com as regras do Sonar: ${qualityGate.status}"
                 }
             }
+           }
         }
 
 }
