a7a24d2700b5f712718583be56ea887e2a76be48
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-01 17:45:46 -0300

    update sonarqube

--- teste1.groovy
+++ teste1.groovy
@@ -21,17 +21,12 @@ node('master'){
         currentBuild.result = 'FAILURE'
         throw new Exception(exec)
         
-    }finally{      
- 
-      print 'Compilação Funcionou!!!! o/'
-       
+    }finally{           
     } 
+
     stage('SonarQube analysis') {
-
 			withSonarQubeEnv('sonarqube') {
-
-				sh 'mvn sonar:sonar -Dsonar.projectKey=projetotask2 -Dsonar.host.url=http://localhost:9000 -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46'
-
+ 				sh 'mvn sonar:sonar -Dsonar.projectKey=projetotask2 -Dsonar.host.url=https://host.docker.internal:9000 -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46'
 			}
 
 		}
