b348075d07e688d9cf1c8630ac21ffe76cfbc88f
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-02 09:32:14 -0300

    TEste sonarqube 0203

--- teste1.groovy
+++ teste1.groovy
@@ -28,5 +28,12 @@ node('master'){
 			withSonarQubeEnv("sonarqube") {
  				sh 'mvnw sonar:sonar -Dsonar.projectKey=projetotask2 -Dsonar.host.url=https://host.docker.internal:9000 -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46'
 			}
-		}
+		
+
+        sh """
+   ${scannerHome}/bin/sonar-scanner \
+   -Dsonar.projectKey=projetotask2 \
+   -Dsonar.sources=0402c6931ae5d3562aa79282d31fe30d5910ff46. \
+"""
+    }
 }
