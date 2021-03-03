7d5289e4a4481b803273cca2aed8dc77e34f11ef
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-03 14:37:04 -0300

    https

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -26,11 +26,11 @@ node('master'){
 
     stage('SonarQube analysis') {
 			withSonarQubeEnv("sonarqube") {
- 			def verifySonarqube = sh returnStatus: true, script: "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=https://localhost:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
+ 			def verifySonarqube = sh returnStatus: true, script: "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
             print verifySonarqube
             if(verifySonarqube != 0){
             echo '1 executando logica https'     
-                sh  "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://localhost:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
+                sh  "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
  	    	echo '2 executando logica https' 
             }
     
