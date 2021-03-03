eacac9bcb6c2924ed0346e30e1e77eb58f2d3a8b
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-03 13:18:09 -0300

    005

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,25 +7,15 @@ node('master'){
                     notFailBuild: true,
                     patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
                                [pattern: '.propsfile', type: 'EXCLUDE']])
- 
-       }
+        }
  
         stage('Compilação do maven e Teste'){
             checkout scm
             sh 'chmod +x mvnw'
             sh './mvnw clean compile package test'
         }
-     
- 
-    }catch (exec)  {
-        currentBuild.result = 'FAILURE'
-        throw neAw Exception(exec)
-    }  
-    
-    finally{        
-            
-    } 
-    stage('SonarQube analysis') {
+
+        stage('SonarQube analysis') {
 			withSonarQubeEnv("sonarqube") {
                 def verifySonarqube = sh (script: "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=https://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46",returnStdout: true).trim()
                 print verifySonarqube
@@ -34,12 +24,23 @@ node('master'){
  	    		 //sh './mvnw sonar:sonar -Dsonar.projectKey=projetotask2 -Dsonar.host.url=https://host.docker.internal:9000 -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46'
                 }
             }
-    }
-    stage('Armazenar imagem no Nexus') {
-        sh 'docker images'
-        //sh 'apt remove golang-docker-credential-helpers'
-        //sh 'docker login localhost:8083 -u admin -p admin'
-        //sh 'docker login localhost:8083'
-        sh 'docker push localhost:8083/petclinic:1.0'
-    }   
+        }
+
+        stage('Armazenar imagem no Nexus') {
+            sh 'docker images'
+            //sh 'apt remove golang-docker-credential-helpers'
+            //sh 'docker login localhost:8083 -u admin -p admin'
+            //sh 'docker login localhost:8083'
+            sh 'docker push localhost:8083/petclinic:1.0'
+        }   
+     
+ 
+    }catch (exec)  {
+        currentBuild.result = 'FAILURE'
+        throw neAw Exception(exec)
+    }  
+    
+    finally{               
+    } 
+   
 }       
