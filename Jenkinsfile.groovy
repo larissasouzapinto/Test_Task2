2a585683e520b23125c38ecb46f4f2636f373f78
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-03 11:57:11 -0300

    Verificação sonarqube http/https

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,21 +19,25 @@ node('master'){
  
     }catch (exec)  {
         currentBuild.result = 'FAILURE'
-        throw new Exception(exec)
-        
+        throw neAw Exception(exec)
     }finally{        
     } 
 
     stage('SonarQube analysis') {
 			withSonarQubeEnv("sonarqube") {
- 				sh './mvnw sonar:sonar -Dsonar.projectKey=projetotask2 -Dsonar.host.url=https://host.docker.internal:9000 -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46'
+
+                def verifySonarqube = sh (script: "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=https://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46",returnStdout: true).trim()
+                print verifySonarqube
+                if(verifySonarqube.contains('can not be reached') == true){
+                    sh  "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
+ 				//sh './mvnw sonar:sonar -Dsonar.projectKey=projetotask2 -Dsonar.host.url=https://host.docker.internal:9000 -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46'
 			}
 		}
 
     stage('Armazenar imagem no Nexus') {
         sh 'docker images'
         //sh 'apt remove golang-docker-credential-helpers'
-        sh 'docker login localhost:8083 -u admin -p admin'
+        //sh 'docker login localhost:8083 -u admin -p admin'
         //sh 'docker login localhost:8083'
         sh 'docker push localhost:8083/petclinic:1.0'
     }   
