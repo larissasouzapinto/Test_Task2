cb157b46e2f4f0f52b0e96686b8c97ce7cb8fd7e
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-04 16:42:13 -0300

    version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -25,7 +25,7 @@ node('master'){
         stage('Armazenar imagem no Nexus') {
             sh 'docker images'
             //sh 'apt remove golang-docker-credential-helpers'
-            sh 'docker build --no-cache -t localhost:8083/petclinic:1.1 .'
+            sh 'docker build --no-cache -t localhost:8083/petclinic:1.${env.BUILD_ID} .'
             sh 'docker login localhost:8083 -u admin -p admin'
             //sh 'docker login localhost:8083'
             sh 'docker push localhost:8083/petclinic:1.1'
