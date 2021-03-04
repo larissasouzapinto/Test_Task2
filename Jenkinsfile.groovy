a367fcaba79cb9ed8e0b6bfbfa8d3485789d95df
Larissa <larissa.souzapinto.lsp@gmail.com>
2021-03-04 13:23:42 -0300

    upload de imagem dockerHub

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,34 +15,67 @@ node('master'){
             sh 'chmod +x mvnw'
             sh './mvnw clean compile package test'
         }
-     
- 
+
+        stage('SonarQube analysis') {
+                withSonarQubeEnv("sonarqube") {
+                /*def verifySonarqube = sh returnStatus: true, script: "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
+                print verifySonarqube
+                if(verifySonarqube != 0){
+                echo '1 executando logica https'     
+                    sh  "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
+                echo '2 executando logica https' 
+                }*/
+                sh  "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=https://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
+        
+            }
+        }    
+
+        stage('Armazenar imagem no Nexus') {
+            sh 'docker images'
+            //sh 'apt remove golang-docker-credential-helpers'
+            sh 'docker login localhost:8083 -u admin -p admin'
+            //sh 'docker login localhost:8083'
+            sh 'docker push localhost:8083/petclinic:1.0'
+            sh 'docker logout'
+        }   
+
+
+        stage('Transformar Conteiners em Imagens'){
+            sh 'docker ps'
+            sh 'docker commit jenkins larissasouzapinto/jobjenkins:lts'
+            sh 'docker commit nexus larissasouzapinto/jobjenkins_nexus:lts'
+            sh 'docker commit sonarqube larissasouzapinto/jobjenkins_sonarqube:lts'
+            sh 'docker commit postgres larissasouzapinto/jobjenkins_postgres:lts'
+            sh 'docker commit infra_nginx-nexusproxy_1 larissasouzapinto/jobjenkins_nginx:lts'   
+        }
+
+        stage('Carregar imagem do Job no DockerHub'){       
+            sh 'docker login -u larissasouzapinto -p adminadmin'
+
+            sh 'docker tag larissasouzapinto/jobjenkins:lts larissasouzapinto/task2:jobjenkins'
+            sh 'docker push larissasouzapinto/task2:jobjenkins'
+
+            sh 'docker tag larissasouzapinto/jobjenkins_nexus:lts larissasouzapinto/task2:jobjenkins_nexus'
+            sh 'docker push larissasouzapinto/task2:jobjenkins_nexus'
+
+            sh 'docker tag larissasouzapinto/jobjenkins_sonarqube:lts larissasouzapinto/task2:jobjenkins_sonarqube'
+            sh 'docker push larissasouzapinto/task2:jobjenkins_sonarqube'
+
+            sh 'docker tag larissasouzapinto/jobjenkins_postgres:lts larissasouzapinto/task2:jobjenkins_postgres'
+            sh 'docker push larissasouzapinto/task2:jobjenkins_postgres'
+
+            sh 'docker tag larissasouzapinto/jobjenkins_nginx:lts larissasouzapinto/task2:jobjenkins_nginx'
+            sh 'docker push larissasouzapinto/task2:jobjenkins_nginx'
+
+            sh 'docker logout'
+
+        }
+
     }catch (exec)  {
         currentBuild.result = 'FAILURE'
         throw new Exception(exec)
         
     }finally{        
-    } 
-
-    stage('SonarQube analysis') {
-			withSonarQubeEnv("sonarqube") {
- 			/*def verifySonarqube = sh returnStatus: true, script: "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
-            print verifySonarqube
-            if(verifySonarqube != 0){
-            echo '1 executando logica https'     
-                sh  "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=http://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
- 	    	echo '2 executando logica https' 
-            }*/
-            sh  "./mvnw sonar:sonar -Dsonar.projectKey=projetotask2  -Dsonar.host.url=https://host.docker.internal:9000   -Dsonar.login=0402c6931ae5d3562aa79282d31fe30d5910ff46"
-    
-		}
-    }    
-
-    stage('Armazenar imagem no Nexus') {
-        sh 'docker images'
-        //sh 'apt remove golang-docker-credential-helpers'
-        sh 'docker login localhost:8083 -u admin -p admin'
-        //sh 'docker login localhost:8083'
-        sh 'docker push localhost:8083/petclinic:1.0'
-    }   
+     
+     }  
 }       
